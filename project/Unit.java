55814769116447c00f6ebf553ea4c90c21a92139
Jzhao123 <zhaojyl935@gmail.com>
2016-05-13 15:56:18 -0700

    Update Unit.java

--- project/Unit.java
+++ project/Unit.java
@@ -4,46 +4,45 @@
  * 
  */
 public class Unit {
-    private int maxhp, hp;
-    private int atk, def;
-    private int exp;
-    private int x, y;
+    private double maxhp, hp;
+    private double atk, def;
+    private double exp;
+    private double xPos, yPos;
 
     /**
      * Constructor for objects of class Unit
      */
-    public Unit(int health, int attack, Map gameMap) {
+    public Unit(double health, double attack, Map gameMap) {
         hp = maxhp = health;
         atk = attack;
         def = 0;
         exp = 0;
     }
-
     /**
      * Returns the amount of remaining health 
      */
-    public int getHealth() {
+    public double getHealth() {
         return hp;
     }
     
     /**
      * Returns the attack stat of the unit
      */
-    public int getAttack() {
+    public double getAttack() {
         return atk;
     }
     
     /**
      * Returns the defense stat of the unit
      */
-    public int getDefense() {
+    public double getDefense() {
         return def;
     }
     
     /**
      * Takes damage. Returns whether or not the unit is still alive.
      */
-    public boolean hit(int damage) {
+    public boolean hit(double damage) {
         hp -= damage;
         return (hp > 0);
     }
