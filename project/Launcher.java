bdc37cbc9f8080261f5c77fa60b003c637ccf20c
Andy Peng <andypg20@gmail.com>
2016-06-03 18:31:04 -0700

    driver

---
+++ project/Launcher.java
@@ -0,0 +1,11 @@
+
+/**
+ * launches the game.
+ */
+public class Launcher {
+    private static void main(String[] args) {
+        GameWindow window = new GameWindow();
+        window.init();
+        window.run();
+    }
+}
